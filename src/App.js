1d8608aaf0ad2678ba7519b3c2294c6965af0c7b
Berkin <c1911073@student.cankaya.edu.tr>
2023-09-29 13:28:45 +0300

    Header Body Footer Done

--- src/App.js
+++ src/App.js
@@ -9,45 +9,39 @@ import { useDataLayerValue } from './DataLayer';
 const spotify = new SpotifyWebApi();
 
 function App() {
-const [{ user, token}, dispatch] = useDataLayerValue();
-  
+  const [{ user, token }, dispatch] = useDataLayerValue();
+
   useEffect(() => {
     const hash = getTokenFromUrl();
     window.location.hash = "";
     const _token = hash.access_token;
-
-    if(_token) {
+    if (_token) {
       dispatch({
-        type: 'SET_TOKEN',
+        type: "SET_TOKEN",
         token: _token,
       });
-      
-
-
+      console.log("[token]", token);
       spotify.setAccessToken(_token);
-      spotify.getMe().then(user => {
-        console.log("PERSON ", user);
-
+      spotify.getMe().then((user) => {
         dispatch({
-          type: 'SET_USER',
-          user: user,
+          type: "SET_USER",
+          user,
         });
       });
-
       spotify.getUserPlaylists().then((playlists) => {
         dispatch({
           type: "SET_PLAYLISTS",
-          playlists: playlists,
+          playlists,
         });
-
+      });
+      spotify.getPlaylist("37i9dQZEVXcJWpXT95GhV7").then((playlist) => {
+        dispatch({
+          type: "SET_DISCOVER_WEEKLY",
+          discover_weekly: playlist,
+        });
       });
     }
-
-    
   }, []);
-
-  console.log("USER >>> ", user);
-  console.log("TOKEN >>> ", token);
 
   return (
     <div className="app"> 
